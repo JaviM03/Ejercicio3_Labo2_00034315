d41dff7d13efaef7a282a04f32b59b407093583d
Javier Martinez <00034315@uca.edu.sv>
2019-03-16 01:19:21 -0600

    asdaf

--- app/src/main/java/com/example/ejercicio3/MainActivity.java
+++ app/src/main/java/com/example/ejercicio3/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.ejercicio3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Tv1;
+    private TextView Tv2;
+    private TextView Tv3;
+    private LinearLayout Ll2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Tv1=findViewById(R.id.tv1);
+        Tv2=findViewById(R.id.tv2);
+        Tv3=findViewById(R.id.tv3);
+        Ll2=findViewById(R.id.Ll2);
+
+        Tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
